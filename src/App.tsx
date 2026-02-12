e6660029ea6e53cfed9395cf1998c47197b91cc3
Srikant Loric <srikantloric@gmail.com>
2026-02-12 22:48:07 +0530

    live feed added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { RouterProvider } from 'react-router-dom';
 import router from '@/routes';
 
 // auth-provider
-// import { FirebaseProvider as AuthProvider } from '@/contexts/FirebaseContext';
 import { AWSCognitoProvider as AuthProvider } from '@/contexts/AWSCognitoContext';
 import { ThemeProvider } from './components/theme-provider';
 
